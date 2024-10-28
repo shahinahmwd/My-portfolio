cd18d60344231a86ff638c62bd7fa397f7035ec4
mdshahinahmed <smshahinahmed4115@gmail.com>
2024-10-28 11:11:38 +0600

    added images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,9 +14,9 @@ const Hero = () => {
       className="relative min-h-screen py-20 lg:py-52 font-Roboto h-screen bg-hero-background bg-cover bg-center bg-no-repeat flex flex-col justify-center items-center text-center text-white"
     >
       <div className="flex flex-col lg:flex-row items-center w-full max-w-6xl mx-auto p-4 lg:p-0">
-        <div className="lg:w-1/2 w-full p-4">
+        <div className="lg:w-1/2 w-full p-4 ">
           <img
-            className="w-full h-auto max-h-96 lg:max-h-full object-contain mx-auto "
+            className="w-full h-auto max-h-96 lg:max-h-full object-contain mx-auto"
             src="/Image/download (2).png"
             alt="Developer"
           />
